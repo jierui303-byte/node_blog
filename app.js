336d98fe19bb006bba858cf8b6916c1c7f922f38
jierui303 <jierui303@163.com>
2017-01-14 05:38:05 +0800

    上传项目初始化

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.disable('trust proxy');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
